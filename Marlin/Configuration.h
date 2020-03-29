65d8db1bc1b9d25d593b3119a20f9f20a7905c13
Gianmauro Cuccuru <gmauro@gmail.com>
2020-03-29 21:47:23 +0200

    PID Tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -485,13 +485,16 @@
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
   #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
+  #define DEFAULT_Kp 20.42
+  #define DEFAULT_Ki 1.34
+  #define DEFAULT_Kd 77.80
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
